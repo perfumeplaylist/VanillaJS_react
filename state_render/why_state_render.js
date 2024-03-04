a84b680b599634106e5269539f08582218b84fc3
kim <perfumeplaylist.gmail.com>
2024-03-04 21:46:00 +0900

    feat: 상태 기반 렌더링의 중요성

--- state_render/why_state_render.js
+++ state_render/why_state_render.js
@@ -2,6 +2,11 @@ const app = document.querySelector("#app");
 
 let count = 0;
 const text = document.createElement("p");
+const text2 = document.createElement("p");
+const text2 = document.createElement("p");
+const text2 = document.createElement("p");
+const text2 = document.createElement("p");
+const text2 = document.createElement("p");
 text.innerHTML = `Count: ${count}`;
 
 const btn = document.createElement("button");
@@ -9,7 +14,13 @@ btn.innerHTML = "+";
 
 btn.addEventListener("click", () => {
   count++;
+  // 렌더링 하는게 많아 진다면???!!
   text.innerHTML = `Count: ${count}`;
+  text2.innerHTML = `Count: ${count}`;
+  text2.innerHTML = `Count: ${count}`;
+  text2.innerHTML = `Count: ${count}`;
+  text2.innerHTML = `Count: ${count}`;
+  text2.innerHTML = `Count: ${count}`;
   app.appendChild(text);
   app.appendChild(btn);
 });
