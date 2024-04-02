a7f96f163238d464d619a9923b3ab03d7231e1a3
kim <perfumeplaylist.gmail.com>
2024-04-02 21:01:11 +0900

    feat: component 추상화

---
+++ state_render/core.js
@@ -0,0 +1,30 @@
+export default class Component {
+  constructor({ target, initalState }) {
+    this.target = target;
+    this.init(initalState);
+    this.render();
+    this.mounted();
+  }
+
+  setState(nextState) {
+    this.state = { ...this.state, ...nextState };
+    this.render();
+  }
+
+  template() {
+    return;
+  }
+
+  setEvent() {}
+
+  render() {
+    this.target.innerHTML = this.template();
+    this.setEvent();
+  }
+
+  init(initalState = {}) {
+    this.state = initalState;
+  }
+
+  mounted() {}
+}
